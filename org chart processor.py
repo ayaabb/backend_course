538f02d910ffb400ffa622f4c99280b05bad0980
ayaabb <ayaabbas011@gmail.com>
2024-03-01 01:52:01 +0200

    fixed something

--- org chart processor.py
+++ org chart processor.py
@@ -1,18 +1,18 @@
 import json
 
 
-def count_workers(dict_org, chief_pos, found_chief, counter, position):
+def workers_counter(dict_org, chief_pos, found_chief, counter, position):
     if found_chief == 0 and chief_pos in dict_org['name']:
 
-        counter = count_workers(dict_org, chief_pos, 1, counter, position)
+        counter = workers_counter(dict_org, chief_pos, 1, counter, position)
     else:
         for x in dict_org['subordinates']:
             if found_chief == 0:
                 if chief_pos in x['name']:
-                    counter = count_workers(x, chief_pos, 1, counter, position)
+                    counter = workers_counter(x, chief_pos, 1, counter, position)
             else:
                 if 'subordinates' in x:
-                    counter = count_workers(x, chief_pos, found_chief, counter, position)
+                    counter = workers_counter(x, chief_pos, found_chief, counter, position)
                 else:
                     if position != '':
                         if position in x['name']:
@@ -38,10 +38,10 @@ j = j.read()
 dict_ = json.loads(j)
 i = 1
 for d in dict_:
-    print(f'Company {i}\nThe number of the workers is {count_workers(dict_[d], "CEO", 0, 0, "")}')
-    print(f'The number of the workers under CTO is {count_workers(dict_[d], "CTO", 0, 0, "")}')
+    print(f'Company {i}\nThe number of the workers is {workers_counter(dict_[d], "CEO", 0, 0, "")}')
+    print(f'The number of the workers under CTO is {workers_counter(dict_[d], "CTO", 0, 0, "")}')
     print(
-        f'The number of the people with “developer” in their title is {count_workers(dict_[d], "CEO", 0, 0, "Developer")}')
+        f'The number of the people with “developer” in their title is {workers_counter(dict_[d], "CEO", 0, 0, "Developer")}')
     print('The names of the departments:')
     print(f'The number of the departments is {department_counter(dict_[d], 0)}\n')
     i += 1
