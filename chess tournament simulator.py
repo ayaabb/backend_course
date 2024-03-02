5309fbfe7d7902bc45bd876849e370c88dcb50a9
ayaabb <ayaabbas011@gmail.com>
2024-03-03 01:39:58 +0200

    chess tournament simulator exercise

--- chess tournament simulator.py
+++ chess tournament simulator.py
@@ -20,23 +20,32 @@ c = 0
 for j in range(3):
     print(f"Round {j + 1} :")
     for i in range(2):
-        reversed_ratio = 1 - (players[players_per_round[c+i][0]].ranking / players[players_per_round[c+i][1]].ranking)
-        print(f"Player {players[players_per_round[c+i][0]].name} vs Player {players[players_per_round[c+i][1]].name}\n"
-              f"player {players_per_round[c+i][0]+1} ranking is {players[players_per_round[c+i][0]].ranking} \n"
-              f"player {players_per_round[c+i][1]+1} ranking is {players[players_per_round[c+i][1]].ranking}\n"
-              f"the ratio = {1-reversed_ratio}\n"
-              f"reversed ratio= {reversed_ratio}\n"
-              f"player {players_per_round[c+i][0]+1} chances of winning = {0.4 - reversed_ratio}\n"
-              f"player {players_per_round[c+i][1]+1} chances of winning = {0.4 + reversed_ratio}\n")
+        reversed_ratio = 1 - (
+                    players[players_per_round[c + i][0]].ranking / players[players_per_round[c + i][1]].ranking)
+        print(
+            f"Player {players[players_per_round[c + i][0]].name} vs Player {players[players_per_round[c + i][1]].name}\n"
+            f"player {players_per_round[c + i][0] + 1} ranking is {players[players_per_round[c + i][0]].ranking} \n"
+            f"player {players_per_round[c + i][1] + 1} ranking is {players[players_per_round[c + i][1]].ranking}\n"
+            f"the ratio = {1 - reversed_ratio}\n"
+            f"reversed ratio= {reversed_ratio}\n"
+            f"player {players_per_round[c + i][0] + 1} chances of winning = {0.4 - reversed_ratio}\n"
+            f"player {players_per_round[c + i][1] + 1} chances of winning = {0.4 + reversed_ratio}\n")
         if reversed_ratio == 0:
             print("Draw!!")
-            players[players_per_round[c+i][0]].total_points += 0.5
-            players[players_per_round[c+i][1]].total_points += 0.5
+            players[players_per_round[c + i][0]].total_points += 0.5
+            players[players_per_round[c + i][1]].total_points += 0.5
         elif reversed_ratio > 0:
-            print(f"The winner of this round is player {players_per_round[c+i][1]+1}")
-            players[players_per_round[c+i][1]].total_points += 1
+            print(f"The winner of this round is player {players_per_round[c + i][1] + 1}")
+            players[players_per_round[c + i][1]].total_points += 1
         else:
-            print(f"The winner of this round is player {players_per_round[c+i][0]+1}")
-            players[players_per_round[c+i][0]].total_points += 1
+            print(f"The winner of this round is player {players_per_round[c + i][0] + 1}")
+            players[players_per_round[c + i][0]].total_points += 1
         print("\n")
-    c+=2
+    c += 2
+max_points=0
+winner_player=""
+for p in players:
+    if p.total_points>max_points:
+        max_points=p.total_points
+        winner_player=p.name
+print(f"The winner of this tournament is {winner_player} with total points {max_points}")
