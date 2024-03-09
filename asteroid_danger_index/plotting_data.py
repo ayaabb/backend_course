6db52f051953d6091cbe8fecdaf2f44824bff951
ayaabb <ayaabbas011@gmail.com>
2024-03-09 02:37:31 +0200

    added the implementation of the extra exercise

--- asteroid_danger_index/plotting_data.py
+++ asteroid_danger_index/plotting_data.py
@@ -3,17 +3,19 @@ import pandas as pd
 import streamlit as st
 
 
-def plot_barChart(values, names):  # plotting the danger index for each asteroid
+def plot_barChart(values, names,week_number):  # plotting the danger index for each asteroid
     values_names = pd.DataFrame({'names': names, 'danger index': values})
 
     st.bar_chart(values_names.set_index('names'))
+    st.title(f"Week{week_number}")
     st.pyplot()
 
 
-def plot_graph(asteroids, X_key, Y_key):  # plotting the relation between two factors
+def plot_graph(asteroids, X_key, Y_key,week_number):  # plotting the relation between two factors
     x = [ast[X_key] for ast in asteroids]
     y = [ast[Y_key] for ast in asteroids]
     df = pd.DataFrame({X_key: x, Y_key: y})
 
     chart = alt.Chart(df).mark_circle().encode(x=X_key, y=Y_key, tooltip=[X_key, Y_key]).interactive()
+    st.title(f"Week{week_number}")
     st.write(chart)
--- asteroid_danger_index/scrape_data.py
+++ asteroid_danger_index/scrape_data.py
@@ -1,13 +1,28 @@
+import asyncio
 import json
-import requests
+import aiofiles
+import aiohttp
 
 
-def get_and_save_data(url_nasa, key):  # requesting data and saving it in json file
-    response = requests.get(url_nasa + key).json()
-    with open("asteroids.json", "w") as file:
-        json.dump(response, file)
+async def run_parallel(
+        parameters_list):  # creating asynchronous tasks for each set of parameters of each week and run them concurrently
+    async with aiohttp.ClientSession() as session:  # create an asynchronous HTTP client session
+        tasks = [asyncio.create_task(get_and_save_data(session, *params)) for params in
+                 parameters_list]
+        data_weeks_list = await asyncio.gather(*tasks)
+    return data_weeks_list
 
-    return response
+
+async def get_and_save_data(session, url_nasa_key,
+                            week_number):  # making an asynchronous web request of a specific week and save the data to a file
+    print('save data week' + week_number + ' started')
+    async with session.get(url_nasa_key) as response:
+        data = await response.json()
+
+    async with aiofiles.open(f'week{week_number}.json', 'w') as file:
+        await file.write(json.dumps(data))
+    print('week' + week_number + ' ended')
+    return data
 
 
 def get_details(data):  # processing the data and saving the required details
@@ -25,3 +40,17 @@ def get_details(data):  # processing the data and saving the required details
             }
             asteroid_data.append(asteroid_info)
     return asteroid_data
+
+
+def week_url(url, week_num, year_and_month, api_key):  # creating a URL of a specific week
+    first_day = (week_num - 1) * 7 + 1
+    last_day = first_day + 6
+    if first_day < 10:
+        first_day = '0' + str(first_day)
+    else:
+        first_day = str(first_day)
+    if last_day < 10:
+        last_day = '0' + str(last_day)
+    else:
+        last_day = str(last_day)
+    return url + 'start_date=' + year_and_month + first_day + '&end_date=' + year_and_month + last_day + '&api_key=' + api_key
